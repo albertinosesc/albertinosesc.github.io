34bb8d1e36942c5a1823ba0b149e608aa19af221
albertinosesc <142363342+albertinosesc@users.noreply.github.com>
2024-08-26 17:14:47 -0300

    Add files via upload

---
+++ plano de aula/script.js
@@ -0,0 +1,12 @@
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwx0XlcpephCu678wxDOYuPTdyBNfXgXXi-GW9wrdAZQIR2uWD037PjDHfL15ay_fA8/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+ e.preventDefault()
+ fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+ .then(response => alert("Thank you! your form is submitted successfully." ))
+ .then(() => { window.location.reload(); })
+ .catch(error => console.error('Error!', error.message))
+})
